e881484e4dce46fca6dfdf1eee0009b3140bbcfa
HARSHINI VINNAKOTA <82930198+v-harshini15@users.noreply.github.com>
2024-01-17 12:46:55 +0530

    Add files via upload

---
+++ pascaltriangle.py
@@ -0,0 +1,15 @@
+def generate(numRows):
+    result = []
+
+    for i in range(numRows):
+        row = [1] * (i + 1)
+        for j in range(1, i):
+            row[j] = result[i-1][j-1] + result[i-1][j]
+        result.append(row)
+
+    return result
+
+# Example
+numRows = 5
+result = generate(numRows)
+print(result)
